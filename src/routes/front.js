33e49cf70292dcaf522e866311a8f2d298131642
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2022-04-01 16:33:16 +0300

    added patch request handling

--- src/routes/front.js
+++ src/routes/front.js
@@ -4,32 +4,43 @@ const router = express.Router()
 
 router.use('/books/info/:id', async(req, res)=>{
     const id = req.params.id
-    const url = 'http://127.0.0.1:3000/books/info/'+id
+    const url = 'http://10.211.55.3/books/info/'+id
     request({url, json:true}, (error,  {body})=>{
            if(error){
-               return res.staus(404).send(error)
+               return res.status(404).send(error)
            }
            return res.status(200).send(body)
         })
 })
 router.use('/books/search/:topic', (req, res)=>{
     const topic = req.params.topic
-    const url = 'http://127.0.0.1:3000/books/search/'+topic
+    const url = 'http://10.211.55.3/books/search/'+topic
     request({url, json:true}, (error,  {body})=>{
            if(error){
-               return res.staus(404).send(error)
+               return res.status(404).send(error)
            }
-           return res.staus(200).send(body)
+           return res.status(200).send(body)
+        })
+})
+router.patch('/books/:id', (req, res)=>{
+    const id = req.params.id
+    const url = 'http://10.211.55.3/books/'+id
+    request({url, json:true, method: 'PATCH', body: req.body}, (error,  {body})=>{
+            if(error){
+               return res.status(404).send(error)
+           }
+           return res.status(200).send(body)
         })
 })
 router.use('/books/purchase/:id', (req, res)=>{
     const id = req.params.id
-    const url = 'http://127.0.0.1:3000/books/purchase/'+id
+    const url = 'http://10.211.55.4/books/purchase/'+id
     request({url, json:true}, (error, {body})=>{
         if(error){
             return res.send(error)
         }
-        return res.status(200).body
+        console.log(body)
+        return res.status(200).send(body)
     })
 })
 
