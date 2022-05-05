db70c3e3c58034e97b28ada5175405ab955c920c
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2022-05-05 15:50:04 +0300

    added caching and load balancing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const frontRouter = require('./routes/front.js')
+const logPort = require('./middlewares/logPort')
 
 const app = express()
 const PORT = process.env.PORT || 3002
 
 app.use(express.json())
+app.use(logPort)
 app.use(frontRouter)
 
 app.listen(PORT, ()=>{
--- src/routes/front.js
+++ src/routes/front.js
@@ -1,47 +1,90 @@
 const express = require('express')
-const request = require ('request')
+const request = require('request')
 const router = express.Router()
+//for load balancing
+const catalogRep = ["10.211.55.3", "10.211.55.4"]
+const orderRep = ["10.211.55.3", "10.211.55.4"]
+let catalogIndex = 0
+let orderIndex = 0
 
-router.use('/books/info/:id', async(req, res)=>{
+//for caching
+let cache = new Map()
+
+//get by id
+router.use('/books/info/:id',  async (req, res) => {
     const id = req.params.id
-    const url = 'http://10.211.55.3/books/info/'+id
-    request({url, json:true}, (error,  {body})=>{
-           if(error){
-               return res.status(404).send(error)
-           }
-           return res.status(200).send(body)
-        })
-})
-router.use('/books/search/:topic', (req, res)=>{
-    const topic = req.params.topic
-    const url = 'http://10.211.55.3/books/search/'+topic
-    request({url, json:true}, (error,  {body})=>{
-           if(error){
-               return res.status(404).send(error)
-           }
-           return res.status(200).send(body)
-        })
-})
-router.patch('/books/:id', (req, res)=>{
-    const id = req.params.id
-    const url = 'http://10.211.55.3/books/'+id
-    request({url, json:true, method: 'PATCH', body: req.body}, (error,  {body})=>{
-            if(error){
-               return res.status(404).send(error)
-           }
-           return res.status(200).send(body)
-        })
-})
-router.use('/books/purchase/:id', (req, res)=>{
-    const id = req.params.id
-    const url = 'http://10.211.55.4/books/purchase/'+id
-    request({url, json:true}, (error, {body})=>{
-        if(error){
-            return res.send(error)
+    if(cache.has('id_'+ id)){
+        console.log('found in cache')
+        return res.status(200).send(cache.get('id_'+ id))
+    } 
+    console.log('not in cache')
+    catalogIndex>= catalogRep.length -1? catalogIndex =0: catalogIndex+=1
+    const url = 'http://'+catalogRep[catalogIndex]+':3000/books/info/' + id
+    console.log(url)
+    request({ url, json: true }, (error, { body } ={}) => {
+        if (error) {
+            return res.status(404).send(error)
         }
-        console.log(body)
+        console.log('retreived from db')
+        cache.set('id_'+body.itemNumber, body)
+        console.log(cache.size)
+        return res.status(200).send(body)
+    });
+})
+
+//get by topic
+router.use('/books/search/:topic', (req, res) => {
+    const topic = req.params.topic
+    if(cache.has('topic_'+ topic)){
+        console.log('found in cache')
+        return res.status(200).send(cache.get('topic_'+ topic))
+    }
+    console.log('not in cache')
+    catalogIndex>= catalogRep.length -1? catalogIndex =0: catalogIndex+=1
+    const url = 'http://'+catalogRep[catalogIndex]+':3000/books/search/' + topic
+    request({ url, json: true }, (error, { body }={}) => {
+        if (error) {
+            return res.status(404).send(error)
+        }
+        cache.set('topic_'+topic , body)
         return res.status(200).send(body)
     })
+})
+
+//patch a book
+router.patch('/books/:id', (req, res) => {
+    const id = req.params.id
+    catalogIndex>= catalogRep.length? catalogIndex =0: catalogIndex+=1
+    const url = 'http://'+catalogRep[catalogIndex]+':3000/books/' + id
+    request({ url, json: true, method: 'PATCH', body: req.body }, (error, { body, statusCode }={}) => {
+        if (error) {
+            return res.status(404).send(error)
+        }
+        return res.status(statusCode).send(body)
+    })
+})
+
+//purchace a book
+router.use('/books/purchase/:id', (req, res) => {
+    const id = req.params.id
+    orderIndex>= orderRep.length? orderIndex =0: orderIndex+=1
+    const url = 'http://'+orderRep[orderIndex]+'/books/purchase/' + id
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            return res.status(500).send(error)
+        }
+        return res.status(200).send(body)
+    })
+})
+
+//new
+//notified to change 
+router.delete('/invalidate/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if(cache.has(id)){
+        cache.delete(id)
+    }
+    res.status(200).send("OK")
 })
 
 module.exports = router
