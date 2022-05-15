80fabe82ae814a4fe388640f3dec92b25ccde533
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2022-05-15 22:21:07 +0300

    added measurements code and pdf

--- src/index.js
+++ src/index.js
@@ -7,7 +7,32 @@ const PORT = process.env.PORT || 3002
 
 app.use(express.json())
 //app.use(logPort)
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} [STARTED]`)
+    const start = process.hrtime()
+
+    res.on('finish', () => {            
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    res.on('close', () => {
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [CLOSED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    next()
+})
 app.use(frontRouter)
+const getDurationInMilliseconds = (start) => {
+    const NS_PER_SEC = 1e9
+    const NS_TO_MS = 1e6
+    const diff = process.hrtime(start)
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
+
 
 app.listen(PORT, ()=>{
     console.log('Front server ir up and running on port:'+ PORT)
